53fa0506f7c3bd2aed53eee245f9d9f0e5fc57ed
hari-rayapaneni <hari.rayapaneni@gmail.com>
2017-08-08 11:07:49 +0530

    Added two way binding and some funtinality check user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  userName= 'harihara';
-  password= 'harahara';
-
+  userName = '';
+  password = '';
+  user = 'No users found';
+  isEnable= false;
   getPassword() {
     return this.password;
+  }
+  constructor() {
+    /*setTimeout(() => {
+      this.isEnable = true;
+    }, 2000);*/
+  }
+  checkUser(event: any) {
+   /* this.user = event.target.user;*/
+     console.log(this.user + '22222222222');
+    if (this.userName === 'harihara') {
+      this.user = 'User ' + this.userName + ' found and logging in ...!';
+      this.userName = '';
+      this.checkIsUserNameEmpty(event);
+    }else {
+      this.user = 'User ' + this.userName + '  not found ...!';
+    }
+  }
+  checkIsUserNameEmpty(event: any) {
+    console.log( 'test' );
+    if (this.userName !== '') {
+      this.isEnable = true;
+    }else {
+      this.isEnable = false;
+    }
   }
 }
 @Component({
